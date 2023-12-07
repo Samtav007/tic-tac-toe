e070685fefbe4dd0bdcb262932fd703168277ef8
Samtav007 <144683044+Samtav007@users.noreply.github.com>
2023-12-07 15:57:19 +0530

    index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let gameResult=document.getElementById('result')
 let message=document.getElementById("message")
 
 let winc=[
-    [0,4,8],[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8]
+    [0,4,8],[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6]
 ]
 boxElement.forEach((box)=>{
     box.onclick=handleClick
